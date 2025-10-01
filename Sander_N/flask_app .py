3846effb6c2b49e62a0625071835471935559942
S-inimene <sandernikulin11@gmail.com>
2025-10-01 20:44:49 +0300

    Update flask_app .py

--- Sander_N/flask_app .py
+++ Sander_N/flask_app .py
@@ -6,14 +6,13 @@ app = Flask(__name__)
 def index():
     if request.method == 'POST':
         try:
-            km = float(request.form['km'])
-            kutus = float(request.form['kutus'])
-            vastus = kutus / km *100
-            return render_template('avaleht.html', vastus = str(vastus), km = str(km), kutus = str(kutus))
+            kaal = float(request.form['kaal'])
+            kordused = float(request.form['kordused'])
         except:
             return render_template('avaleht.html')
     else:
         return render_template('avaleht.html')
 
 if __name__ == '__main__':
+
     app.run()
