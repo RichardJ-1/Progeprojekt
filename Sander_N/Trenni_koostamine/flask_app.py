5ad8a64de7361986282297ca1e4123c0180b06d1
S-inimene <sandernikulin11@gmail.com>
2025-10-16 09:01:33 +0300

    Update flask_app.py

--- Sander_N/Trenni_koostamine/flask_app.py
+++ Sander_N/Trenni_koostamine/flask_app.py
@@ -1,32 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
-import json
-import os
+from flask import Flask, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
-    if request.method == 'POST':
-        data = request.form.to_dict()
+@app.route('/', methods=['POST'])
+def save_workout():
+    data = request.form.to_dict()
+    with open('tulemus.txt', 'w', encoding='utf-8') as f:
+        for key, value in data.items():
+            f.write(f"{key}: {value}\n")
+    return "Tulemus salvestatud!"
 
-        # salvesta
-        existing_data = []
-        if os.path.exists('andmed.json'):
-            with open('andmed.json', 'r', encoding='utf-8') as f:
-                try:
-                    existing_data = json.load(f)
-                except json.JSONDecodeError:
-                    existing_data = []
-
-        existing_data.append(data)
-        with open('andmed.json', 'w', encoding='utf-8') as f:
-            json.dump(existing_data, f, ensure_ascii=False, indent=4)
-
-        return redirect(url_for('index'))  # pärast salvestamist tagasi lehele
-
-    return render_template('trennileht.html')
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
 
