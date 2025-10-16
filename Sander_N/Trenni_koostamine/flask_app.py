d309d9f57618c063e149d7b2e3d3e8e542009153
S-inimene <sandernikulin11@gmail.com>
2025-10-16 09:52:01 +0300

    Update flask_app.py

--- Sander_N/Trenni_koostamine/flask_app.py
+++ Sander_N/Trenni_koostamine/flask_app.py
@@ -1,13 +1,25 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('trennileht.html')
+
 @app.route('/', methods=['POST'])
-def save_workout():
+def save_results():
     data = request.form.to_dict()
+
+    # Kirjuta kõik väljad faili
     with open('tulemus.txt', 'w', encoding='utf-8') as f:
+        f.write("TREENINGU TULEMUSED:\n\n")
         for key, value in data.items():
             f.write(f"{key}: {value}\n")
-    return "Tulemus salvestatud!"
+
+    return "<h2>Tulemus salvestatud faili 'tulemus.txt'!</h2><br><a href='/'>Tagasi</a>"
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
 
 
